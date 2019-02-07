ece84cc75990e7071791cdc8880d6e50d31d5ab5
Shafi Hoque <shafihoque@Shafis-MacBook-Air.local>
2019-02-07 15:44:11 -0800

    First commit

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+// Typescript doesn't know where is that componenet coming from, 
+// so we need to import it
+@Component({
+    //we are passing javascript object to configure it
+    //it or the naming of the selector should be unique 
+    //so that we don't override the default one
+    //Best practice is to name it app-some_name
+
+    //The templateUrl is an external file needs to be created in the same
+    //folder
+    //here we are creating a server.componenet.html file inside our server folder
+
+    //give the templateUrl a relative path to the servercomponent.html
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+export class ServerComponent{}
