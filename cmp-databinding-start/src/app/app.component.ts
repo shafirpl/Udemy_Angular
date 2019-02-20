0dbe52a97b244c06bf005acc554ef4c893a7aa17
shafirpl <shafi.rpl@gmail.com>
2019-02-20 02:18:36 -0800

    Event emitter

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,23 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{type: 'server', name: 'Testserver',
+content: 'Just a Test!'}];
 
-  onAddServer() {
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
     this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
     });
   }
 
-  onAddBlueprint() {
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
     this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
     });
   }
 }
