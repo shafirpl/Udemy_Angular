b4c5682ad30f165e8737ad8d3f429ac0d3373d43
shafirpl <shafi.rpl@gmail.com>
2019-03-25 22:20:10 -0700

    Working

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
@@ -16,7 +16,21 @@ export class ServerComponent implements OnInit {
     private route: ActivatedRoute) {}
 
   ngOnInit() {
+    /*
+    * We are adding + here to convert the string to number, because
+    * in our definition, we have numbers, if we didn't convert it to a number, we
+    * would get something like /servers/'1', and our server won't be able to find it
+    * That is why in order to change '1' to 1, we need to convert it to number
+    */
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(1);
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          //similar reason discussed above, converting string to number
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
   }
 
 
